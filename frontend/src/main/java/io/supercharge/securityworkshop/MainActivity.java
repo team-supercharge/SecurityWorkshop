6ef39abfb3192f01bdcb6835b82160408dc1263b
Csaba Kozak <csaba.kozak@supercharge.io>
2017-11-30 09:14:27 +0100

    Prepare SafetyNet Demo

--- frontend/src/main/java/io/supercharge/securityworkshop/MainActivity.java
+++ frontend/src/main/java/io/supercharge/securityworkshop/MainActivity.java
@@ -4,19 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
-import android.util.Log;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.safetynet.SafetyNet;
-import com.google.android.gms.safetynet.SafetyNetClient;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Random;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 
@@ -32,8 +22,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setupViews();
-
-        safetynetCheck();
     }
 
     private void setupViews() {
@@ -73,57 +61,6 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                     finish();
                 }, Throwable::printStackTrace);
-    }
-
-    private void safetynetCheck() {
-        if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
-                != ConnectionResult.SUCCESS) {
-
-            Log.e(TAG, "Google Play Services is not available!");
-
-            return;
-        }
-
-        String nonceData = "Safety Net: " + System.currentTimeMillis(); // this should come from server
-
-        byte[] nonce = getRequestNonce(nonceData);
-
-        SafetyNetClient client = SafetyNet.getClient(this);
-
-        client.attest(nonce, BuildConfig.SAFETYNET_API_KEY)
-                .addOnSuccessListener(this, attestationResponse -> {
-                    String result = attestationResponse.getJwsResult();
-
-                    Log.i(TAG, decodeJws(result));
-
-                    Api api = ApiManager.getInstance().getApi();
-
-                    // we should call the our trusted server
-                    AppVerificationRequest request = new AppVerificationRequest();
-                    request.setSignedAttestation(result);
-
-                    api.verifyApp(BuildConfig.SAFETYNET_API_KEY, request)
-                            .subscribe(appVerificationResponse
-                                    -> Log.i(TAG, "verified: " + appVerificationResponse.isValidSignature()));
-                })
-                .addOnFailureListener(this, e -> Log.e(TAG, "SafetyNet error", e));
-    }
-
-    private byte[] getRequestNonce(String data) {
-        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-        byte[] bytes = new byte[24];
-
-        Random random = new Random();
-
-        random.nextBytes(bytes);
-        try {
-            byteStream.write(bytes);
-            byteStream.write(data.getBytes());
-        } catch (IOException e) {
-            return null;
-        }
-
-        return byteStream.toByteArray();
     }
 
     public String decodeJws(String jwsResult){
